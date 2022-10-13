f7572b0c4188dd2fcedba31a5d15afc0a9599393
Liwaho Xu <lwhollow@yandex.ru>
2022-10-13 20:50:51 +0500

    Third task

--- Program.cs
+++ Program.cs
@@ -44,7 +44,7 @@ void Print(int[,] array)
 i = 4, j = 2 -> такого числа в массиве нет
 i = 1, j = 2 -> 2*/
 
-int[,] array = new int[3, 5];
+/*int[,] array = new int[3, 5];
 Random(array);
 Print(array);
 Console.WriteLine("Строка: ");
@@ -80,10 +80,59 @@ void Print(int[,] array)
         }
         Console.WriteLine("");
     }
-}
+}*/
+
 /*Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 Например, задан массив:
 1 4 7 2
 5 9 2 3
 8 4 2 4
 Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
+
+Console.WriteLine("Количество строк: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Количество столбцов: ");
+int n = int.Parse(Console.ReadLine());
+int[,] array = new int[m, n];
+Random(array);
+Print(array);
+Console.WriteLine();
+Arifmethic(array);
+
+void Random(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-99, 99);
+        }
+    }
+}
+
+void Print(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine("");
+    }
+}
+
+void Arifmethic(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+{
+    double a = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        a = (a + array[i, j]);
+    }
+    a = a / m;
+    Console.WriteLine("Среднее арифметическое по " + j + " столбцу : " + a + "; ");
+}
+
+}
